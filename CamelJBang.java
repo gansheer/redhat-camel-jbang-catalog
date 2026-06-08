eccd8c9a6c8e6d3a9506b50f0fc82bd0ddbc392c
Croway <federico.mariani.1990@gmail.com>
2026-06-08 10:13:05 +0200

    Update to latest 4.19.0 ER1/CS1 dev build versions

--- CamelJBang.java
+++ CamelJBang.java
@@ -19,10 +19,10 @@
 
 //JAVA 17+
 //REPOS central=https://repo1.maven.org/maven2,redhat.ga=https://maven.repository.redhat.com/ga/
-//JAVA_OPTIONS -Dcamel.jbang.camelSpringBootVersion=4.19.0.redhat-00001
-//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.19.0.redhat-00001}@pom
-//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.19.0.redhat-00001}
-//DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.19.0}
+//JAVA_OPTIONS -Dcamel.jbang.camelSpringBootVersion=4.19.0.redhat-00010
+//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.19.0.redhat-00010}@pom
+//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.19.0.redhat-00010}
+//DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.19.0.redhat-00009}
 
 package main;
 
