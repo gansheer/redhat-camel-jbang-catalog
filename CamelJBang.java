a3533fe6fd01db1e4d78c210bddf5614d8f788a0
Reqour <maitai-bpms@redhat.com>
2026-06-28 07:28:36 +0000

    Reqour

--- CamelJBang.java
+++ CamelJBang.java
@@ -19,9 +19,9 @@
 
 //JAVA 17+
 //REPOS central=https://repo1.maven.org/maven2,redhat.ga=https://maven.repository.redhat.com/ga/
-//JAVA_OPTIONS -Dcamel.jbang.camelSpringBootVersion=4.18.1.redhat-00014 -Dcamel.jbang.quarkusGroupId=com.redhat.quarkus.platform -Dcamel.jbang.quarkusArtifactId=quarkus-bom -Dcamel.jbang.quarkusVersion=3.33.2.SP1-redhat-00001
-//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.18.1.redhat-00019}@pom
-//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.18.1.redhat-00019}
+//JAVA_OPTIONS -Dcamel.jbang.camelSpringBootVersion=4.18.1.redhat-00026
+//DEPS org.apache.camel:camel-bom:${camel.jbang.version:4.18.1.redhat-00038}@pom
+//DEPS org.apache.camel:camel-jbang-core:${camel.jbang.version:4.18.1.redhat-00038}
 //DEPS org.apache.camel.kamelets:camel-kamelets:${camel-kamelets.version:4.18.1.redhat-00021}
 
 package main;
